a64c2539916a76350b4609e1cd2f450b221d27a6
TEDDY TECH <kibetwycliffe093@gmail.com>
2026-04-22 16:47:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,11 @@ class Config:
     OWNER_ID = int(os.getenv("OWNER_ID", 0))
     MONGO_URI = os.getenv("MONGO_URI")
     
-    # Force Subscribe IDs (Should be integers starting with -100)
+    # Force Subscribe IDs (Must be integers)
     FS_CHANNEL = int(os.getenv("FORCE_SUB_CHANNEL", 0))
     FS_GROUP = int(os.getenv("FORCE_SUB_GROUP", 0))
+    
+    # Custom Banner
+    START_IMG = "https://files.catbox.moe/13nyhx.jpg"
     
     DEFAULT_EMOJIS = ['❤️', '🔥', '💫', '👍', '😂', '🎉', '💯', '⚡']
